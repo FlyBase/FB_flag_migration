0bab591a45f920263f5df6f2399bc02e8494c5b6
Gillian Millburn <gm119@cam.ac.uk>
2025-09-12 16:27:04 +0100

    adding subroutine to get AGRKB number from the supplied FBrf number

--- lib/ABCInfo.pm
+++ lib/ABCInfo.pm
@@ -5,7 +5,7 @@ use warnings;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(get_topic_entity_tag_source_data);
+our @EXPORT = qw(get_topic_entity_tag_source_data get_AGRKB_for_FBrf);
 
 use JSON::PP;
 
@@ -27,7 +27,7 @@ sub get_topic_entity_tag_source_data {
 
 	Title:    get_topic_entity_tag_source_data
 	Usage:    get_topic_entity_tag_source_data(API_type, curator_type);
-	Function: The get_topic_entity_tag_source_data subroutine gets json that describes the topic_entity_tag_source information, using a curl command to query the relevant Alliance REST API service
+	Function: The get_topic_entity_tag_source_data subroutine gets json that describes the topic_entity_tag_source information, using a curl command to query the relevant Alliance REST API service, and returns that information as a reference.
 	Example: my $source_data = &get_topic_entity_tag_source_data('production', 'curator');
 
 	
@@ -36,8 +36,6 @@ Arguments:
 o API_type: the 'type' of the relevant Alliance REST API service to query - must be either 'production' or 'stage'
 
 o curator_type: the type of curation that you want the json details for - must be either 'curator' or 'author'
-
-o Returns the appropriate source information as a reference
 
 
 =cut
@@ -52,7 +50,7 @@ o Returns the appropriate source information as a reference
 
 	unless ($API_type eq 'production' | $API_type eq 'stage') {
 
-		die "unrecognized value ($API_type) passed to get_topic_entity_tag_source_data surboutine as API_type: must be 'production' or 'stage'\n";
+		die "unrecognized value ($API_type) passed to get_topic_entity_tag_source_data subroutine as API_type: must be 'production' or 'stage'\n";
 
 
 	}
@@ -106,5 +104,78 @@ o Returns the appropriate source information as a reference
 
 
 	return ($result);
+
+}
+
+
+sub get_AGRKB_for_FBrf {
+
+
+
+=head1 SUBROUTINE:
+=cut
+
+=head1
+
+	Title:    get_AGRKB_for_FBrf
+	Usage:    get_AGRKB_for_FBrf(API_type, FBrf);
+	Function: The get_AGRKB_for_FBrf subroutine gets the corresponding AGRKB number for the FBrf number supplied as the second argument, by using a curl command to query the relevant Alliance REST API service.
+	Example: my $AGRKB_data = &get_AGRKB_for_FBrf('production', 'FBrf0111489');
+
+
+Arguments:
+
+o API_type: the 'type' of the relevant Alliance REST API service to query - must be either 'production' or 'stage'
+
+o FBrf: the FBrf number for which the AGRKB number is needed.
+
+
+Returns:
+
+o Corresponding AGRKB number.
+
+=cut
+
+	unless (@_ == 2) {
+
+		die "Wrong number of parameters passed to the get_AGRKB_for_FBrf subroutine\n";
+	}
+
+	my ($API_type, $FBrf) = @_;
+
+	unless ($API_type eq 'production' | $API_type eq 'stage') {
+
+		die "unrecognized value ($API_type) passed to get_AGRKB_for_FBrf surboutine as API_type: must be 'production' or 'stage'\n";
+
+
+	}
+
+	unless ($FBrf =~ m/^FBrf[0-9]{7}$/) {
+		die "FBrf number passed to get_AGRKB_for_FBrf subroutine ($FBrf) is not correct format\n";
+
+	}
+
+	my $cmd = '';
+
+	if ($API_type eq 'stage') {
+
+		$cmd = "curl -X 'GET' 'https://stage-literature-rest.alliancegenome.org/cross_reference/FB%3A$FBrf' -H 'accept: application/json'";
+
+	} else {
+
+		$cmd = "curl -X 'GET' 'https://literature-rest.alliancegenome.org/cross_reference/FB%3A$FBrf' -H 'accept: application/json'";
+
+	}
+
+# fetch the data and convert the json to a reference
+	my $raw_json =`$cmd`;
+
+	my $json_encoder = JSON::PP->new()->canonical(1);
+	my $result = $json_encoder->decode($raw_json);
+
+	my $AGRKB = $result->{reference_curie};
+
+	return ($AGRKB);
+
 
 }
